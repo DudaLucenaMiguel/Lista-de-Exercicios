ac66e5c0c1ea5ca3d1699a08edc520f51c32489a
Duda Lucena Miguel <duda.lucena@outlook.com>
2025-04-06 20:36:05 -0300

    commit

---
+++ Questao4_Lanche_Facil/app/src/main/java/br/com/lanchefacil/www/TelaDeBoasVindas.java
@@ -0,0 +1,38 @@
+package br.com.lanchefacil.www;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TelaDeBoasVindas extends AppCompatActivity {
+
+    private Button btnContinuar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_tela_de_boas_vindas);
+
+        btnContinuar = findViewById(R.id.btnContinuar);
+
+        btnContinuar.setOnClickListener(view -> {
+            Intent intent = new Intent(this, FormularioDePedidos.class);
+            startActivity(intent);
+            finish();
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
