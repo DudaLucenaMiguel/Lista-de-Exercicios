b457afbd100b3530e4de3d9e6146590438845c4a
Duda Lucena Miguel <duda.lucena@outlook.com>
2025-04-12 19:09:04 -0300

    commit

--- Questao2_Calculadora_de_salario/app/src/main/java/br/com/fecapccp/calculadoradesalrio/MainActivity.java
+++ Questao2_Calculadora_de_salario/app/src/main/java/br/com/fecapccp/calculadoradesalrio/MainActivity.java
@@ -66,4 +66,33 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
 }
